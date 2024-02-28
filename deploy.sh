3fa41a3912c57815333c17f700684a37d2ca61c6
ar0n <asimpole.as@gmail.com>
2024-02-28 00:28:18 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/AgileWebApplication/
-git pull origin master
-yarn install &&
-yarn build &&
+git pull origin main
+npm install &&
+npm run build &&
 pm2 restart AgileWebApplication
