384a203fc232e119e4e9b6e5900dec2873ca48d7
ar0n <asimpole.as@gmail.com>
2024-02-28 00:30:09 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ec2-user/AgileWebApplication/
 git pull origin main
 npm install &&
